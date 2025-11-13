cfe8b8a0b8e7c0da59f35a8bbe06cbbb6f1bf918
TeteuTias <46979510+TeteuTias@users.noreply.github.com>
2025-11-12 21:46:40 -0300

    Update page.tsx

--- app/presenca/gerarListaMinicursoPresenca/[_id]/page.tsx
+++ app/presenca/gerarListaMinicursoPresenca/[_id]/page.tsx
@@ -621,6 +621,14 @@ const ModalUserFound: React.FC<{ courseData: ICourse, qrCodeResult: string, clos
                                             getFeedBack(user, courseData).map((value) => <p key={`${user._id}`} className='text-red-700 font-semibold'>{value}</p>)
                                     )
                                 }
+                                {
+                                    user && courseData.participants.includes(`${user._id}`) ? "O Congressista está inscrito no minicurso" : ""
+                                }
+                                {
+                                    user && courseData.participants.includes(`${user._id}`) &&
+                                        courseData.attendanceList.includes(`${user._id}`)
+                                        ? "O Congressista já possui presença no minicurso" : ""
+                                }
                             </div>
                         </div>
                         <div className='flex flex-col items-center content-center justify-center space-y-1'>
@@ -650,7 +658,7 @@ const ModalUserFound: React.FC<{ courseData: ICourse, qrCodeResult: string, clos
 
                                     }
                                 }
-                                className="w-fit px-4 w-lg py-3 bg-red-600 text-white text-base font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors"
+                                className="w-fit px-4 w-lg py-3 bg-blue-300 text-white text-base font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors"
                             >
                                 Dar Presença
                             </button>
